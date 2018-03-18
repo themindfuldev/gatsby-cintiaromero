8b554ad370730a04e490ae3907e70267e6f2118a
Tiago Garcia <tiagoromerogarcia@gmail.com>
2018-03-18 13:02:53 -0700

    hero image banner on about page

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,37 +3,37 @@ import Header from '../header/Header';
 import './banner.sass';
 
 const Banner = () => (
-	<section class="hero is-fullheight is-light is-bold">
-		<div class="hero-head">
+	<section className="main-hero hero is-fullheight is-light">
+		<div className="hero-head">
 			<Header />
 		</div>
-		<div class="hero-body">
-			<div class="main-banner container has-text-centered">
-				<h1 class="title is-1 is-spaced">
-					I'm Cintia Romero
+		<div className="hero-body">
+			<div className="main-banner container has-text-centered">
+				<h1 className="title is-1 is-spaced">
+					Hi. I'm Cintia Romero
 				</h1>
-				<h2 class="subtitle">
-					I'm a San Francisco based <span>ux designer</span> and <span>visual designer</span> creating awesome and effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a> and learn more <a className="smoothscroll" href="#about">about me</a>.
+				<h2 className="subtitle">
+					I design and develop experiences that make people's life simpler.
 				</h2>
-				<div class="columns is-mobile is-multiline is-centered">
-					<div class="column is-narrow">
-						<a href="#">
-							<i class="fab fa-facebook-square fa-2x"></i>
+				<div className="columns is-mobile is-multiline is-centered">
+					<div className="column is-narrow">
+						<a href="https://www.pinterest.com/cintiaromero313" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-pinterest-square fa-2x"></i>
 						</a>
 					</div>
-					<div class="column is-narrow">
-						<a href="#">
-							<i class="fab fa-twitter-square fa-2x"></i>
+					<div className="column is-narrow">
+						<a href="https://instagram.com/cintiaaaromero" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-instagram fa-2x"></i>
 						</a>
 					</div>
-					<div class="column is-narrow">
-						<a href="#">
-							<i class="fab fa-instagram fa-2x"></i>
+					<div className="column is-narrow">
+						<a href="https://twitter.com/cintiaaa_romero" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-twitter-square fa-2x"></i>
 						</a>
 					</div>
-					<div class="column is-narrow">
-						<a href="#">
-							<i class="fab fa-linkedin fa-2x"></i>
+					<div className="column is-narrow">
+						<a href="https://www.linkedin.com/in/cintiaromero" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-linkedin fa-2x"></i>
 						</a>
 					</div>
 				</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Link from "gatsby-link";
 import Script from "react-load-script";
-import graphql from "graphql";
-
 import Banner from '../components/banner/Banner';
 
 export default class IndexPage extends React.Component {
@@ -20,9 +18,6 @@ export default class IndexPage extends React.Component {
   }
 
   render() {
-    const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
-
     return (
       <div>
         <Banner />
@@ -31,57 +26,8 @@ export default class IndexPage extends React.Component {
             url="https://identity.netlify.com/v1/netlify-identity-widget.js"
             onLoad={this.handleScriptLoad}
           />
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-            </div>
-            {posts
-              .filter(post => post.node.frontmatter.templateKey === "blog-post")
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: "1px solid #eaecee", padding: "2em 4em" }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.frontmatter.path}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.frontmatter.path}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
-          </div>
         </section>
       </div>
     );
   }
 }
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-            path
-          }
-        }
-      }
-    }
-  }
-`;
