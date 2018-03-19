a368587585a708a302e3d803422048178f38722a
Tiago Garcia <tiagoromerogarcia@gmail.com>
2018-03-18 17:21:37 -0700

    fixing build issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,7 @@ module.exports = {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
+    },    
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
