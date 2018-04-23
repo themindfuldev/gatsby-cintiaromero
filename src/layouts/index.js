396945b4a474e8a7b40e8e19c7d6dea957409e6b
Tiago Garcia <tiagoromerogarcia@gmail.com>
2018-04-22 20:16:46 -0700

    adding netlify script

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import Script from "react-load-script";
 import smoothscroll from 'smoothscroll-polyfill';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import './all.sass';
+
+const handleScriptLoad = () => {
+  if (typeof window !== `undefined` && window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+  window.netlifyIdentity.init();
+};
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -14,6 +28,10 @@ const TemplateWrapper = ({ children }) => (
     <Header />
     {children()}
     <Footer />
+    <Script
+      url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+      onLoad={handleScriptLoad}
+    />
   </div>
 );
 
