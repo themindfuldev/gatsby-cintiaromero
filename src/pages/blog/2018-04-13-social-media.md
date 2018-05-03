b266955ee025c8a7aa4ff46b9801fdd9224f5616
Tiago Garcia <tiagoromerogarcia@gmail.com>
2018-05-02 22:16:58 -0700

    logos and social media

--- src/pages/blog/2018-04-13-social-media.md
+++ src/pages/blog/2018-04-13-social-media.md
@@ -9,20 +9,70 @@ description: Diving into the social game, helping brands to go out, speak their 
 image: /img/vd-social-media.jpg
 ---
 
-### Semana off
+### You + me = awesome
 
-This design is for a sales marketing campaign for Regina Salomão.
+![You + me = awesome](/img/vd-social-media-1.jpg)
+
+### We love Boutique-in-a-box
+
+![We love Boutique-in-a-box](/img/vd-social-media-2.jpg)
+
+### Dare to begin
+
+![Dare to begin](/img/vd-social-media-3.jpg)
+
+### Start today
+
+![Start today](/img/vd-social-media-4.jpg)
+
+### Be a goal digger
+
+![ Be a goal digger](/img/vd-social-media-5.jpg)
+
+### Who runs the world?
+
+![Who runs the world?](/img/vd-social-media-6.jpg)
+
+### Keep calm and be thankful
+
+![Keep calm and be thankful](/img/vd-social-media-7.jpg)
+
+### Small business is a big deal
+
+![Small business is a big deal](/img/vd-social-media-8.jpg)
+
+### Doubt
+
+![Doubt](/img/vd-social-media-9.jpg)
+
+### You think I'm wild?
+
+![You think I'm wild?](/img/vd-social-media-10.jpg)
+
+### Complete freedom
+
+![Complete freedom](/img/vd-social-media-12.jpg)
+
+### Happy easter
+
+![Happy easter](/img/vd-social-media-13.jpg)
+
+### You made it!
+
+![You made it!](/img/vd-social-media-11.jpg)
+
+### Congrats new executive director
+
+![Congrats new executive director](/img/vd-social-media-14.jpg)
+
+### I am a woman
+
+![I am a woman](/img/vd-social-media-15.jpg)
+
+### Semana off
 
 ![Semana off](https://farm1.staticflickr.com/696/20860242794_23bb9c7c13_h.jpg)
 
-### Clothes swap banner
-
-This banner was created for a Facebook and Eventbrite event page for a fun clothes swap by Epifania Magazine.
-
-![Clothes swap banner](https://farm8.staticflickr.com/7507/26778621212_8c90c6231c_h.jpg)
-
 ### Mega bazar
-
-This design is for a sales marketing campaign for Regina Salomão.
 
 ![Mega bazar](https://farm1.staticflickr.com/708/21296049659_02b6db1661_h.jpg)
--- src/templates/post-page.js
+++ src/templates/post-page.js
@@ -3,12 +3,13 @@ import graphql from 'graphql';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 import Banner from '../components/banner/Banner';
+import ImageGrid from '../components/image-grid/ImageGrid';
 import Content, { HTMLContent } from '../components/content/Content';
 import { headings } from '../utils/headings';
 import './post-page.sass';
 
 export const PostPageTemplate = ({
-  content, contentComponent, helmet, title, subtitle, description, tags
+  content, contentComponent, helmet, title, subtitle, description, tags, isImageGrid
 }) => {
   const PostContent = contentComponent || Content;
   const tag = tags[0];
@@ -22,22 +23,27 @@ export const PostPageTemplate = ({
         <div className="container content">
           <div className="columns">
             <div className="column is-10 is-offset-1">
-                {description &&
-                  <React.Fragment>
-                    <nav className="breadcrumb" aria-label="breadcrumbs">
-                      <ul>
-                        <li><Link to={`/tags/${tag}`}>{heading.title}</Link></li>
-                        <li className="is-active"><a aria-current="page">{title}</a></li>
-                      </ul>
-                    </nav>
-                    <article className="message">
-                      <div className="message-body">
-                        {description}
-                      </div>
-                    </article>
-                  </React.Fragment>
-                }
-              <PostContent content={content} className="post" />
+              {description &&
+                <React.Fragment>
+                  <nav className="breadcrumb" aria-label="breadcrumbs">
+                    <ul>
+                      <li><Link to={`/tags/${tag}`}>{heading.title}</Link></li>
+                      <li className="is-active"><a aria-current="page">{title}</a></li>
+                    </ul>
+                  </nav>
+                  <article className="message">
+                    <div className="message-body">
+                      {description}
+                    </div>
+                  </article>
+                </React.Fragment>
+              }
+              {isImageGrid &&
+                <ImageGrid images={content} />
+              }
+              {!isImageGrid &&
+                <PostContent content={content} className="post" />
+              }
             </div>
           </div>
         </div>
@@ -48,7 +54,7 @@ export const PostPageTemplate = ({
 
 export default ({ data }) => {
   const { markdownRemark: post } = data;
-  const { title, subtitle, description, tags } = post.frontmatter;
+  const { title, subtitle, description, tags, isImageGrid } = post.frontmatter;
 
   return (<PostPageTemplate
     content={post.html}
@@ -58,6 +64,7 @@ export default ({ data }) => {
     subtitle={subtitle}
     description={description}
     tags={tags}
+    isImageGrid={isImageGrid}
   />);
 };
 
@@ -72,6 +79,7 @@ export const pageQuery = graphql`
         subtitle
         description
         tags
+        isImageGrid
       }
     }
   }
